20549ed1d9e39b92937d72133123d3ee62e3835f
ductuan0701 <ductuan7106@gmail.com>
2026-08-06 02:04:02 +0700

    è

--- hoctructuyen/src/main/java/com/dan/HoctructuyenApplication.java
+++ hoctructuyen/src/main/java/com/dan/HoctructuyenApplication.java
@@ -8,10 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class HoctructuyenApplication {
 
     public static void main(String[] args) {
-        // GIẢ LẬP LỖI NGHIÊM TRỌNG KHI KHỞI ĐỘNG (FATAL ERROR)
-        if (true) {
-            throw new RuntimeException("Fatal Error: Không thể khởi động ứng dụng do sai cấu hình Database!");
-        }
         SpringApplication.run(HoctructuyenApplication.class, args);
     }
 
